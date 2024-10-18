8852e246aa29cd9309149046f217f5888fbab599
Kanne <doantrungkien18@gmail.com>
2024-10-18 21:09:55 +0700

    update script

--- Assets/_asset/script/Move.cs
+++ Assets/_asset/script/Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    int x = 0, y = 0, z = 0;
+    float x = 0, y = 0, z = 0;
     // Start is called before the first frame update
     void Start()
     {
